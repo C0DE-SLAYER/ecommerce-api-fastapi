66d07a884e9c932104e61c1ce7576d3ef99275bb
c0de-slayer <fsali315@gmail.com>
2024-08-20 21:33:06 +0530

    Added the root url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from data.models import Base
 from data.db import engine
 from controllers import include_router
@@ -10,5 +11,13 @@ app = FastAPI()
 
 include_router(app)
 
+@app.get('/')
+def home():
+    html = '''
+        <h1>Welcome to ecommerce api</h1>
+        <p> To Use the api head to the <a href='/docs'>/docs</a> endpoint</p>
+    '''
+
+    return HTMLResponse(html, status_code=200)
 if __name__ == "__main__":
     uvicorn.run(app)
